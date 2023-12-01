fbe48ae128f88dc61d9213d12dbc70a476d2423d
Jianian.Zhang <46586581+iloveyou-github@users.noreply.github.com>
2023-12-01 21:22:43 +0800

    delete core code 1.0

--- core/include/utils/ProtoBuf/protobuf.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PROTOBUF_H__
-#define __PROTOBUF_H__
-
-#include "include/ProtoBuf/package.pb.h"
-#include "include/utils/ProtoBuf/pb_decode.h"
-#include "include/utils/ProtoBuf/pb_encode.h"
-
-#endif
-
