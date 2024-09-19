6ac8c2d5468eded64fecce3dad4e2678330a8d6f
Jianian.Zhang <46586581+iloveyou-github@users.noreply.github.com>
2024-09-19 14:18:15 +0800

    Update pal/devregister code
    :according to 20240918 agreement

--- pal/Linux/DeviceRegister/deviceregister.c
+++ pal/Linux/DeviceRegister/deviceregister.c
@@ -8,6 +8,7 @@
 #include "include/jose/jose.h"
 #include "include/dids/dids.h"
 
+#include "include/utils/convert/convert.h"
 #include "include/utils/devRegister/devRegister.h"
 
 #define PORT 8000
@@ -17,6 +18,13 @@
  
 static char * upload_did = NULL;
 static char * upload_diddoc = NULL;
+
+static uint8_t secret[32] = {0x57, 0x81, 0x5e, 0x3d, 0x20, 0x9a, 0x42, 0x8d, 0x48, 0x44, 0x83, 0xcc, 0x1a, 0x2c, 0x5b, 0x5d, 0x97, 0x00, 0x7d, 0x5f, 0x17, 0xff, 0xc0, 0xd4, 0xee, 0xd6, 0x03, 0xa4, 0x08, 0x55, 0x03, 0x9e};
+
+static psa_key_id_t device_register_key_id = 0;
+static uint8_t signature[64] = {0};
+static char    signature_str[64 * 2 + 1] = {0};
+
 
 static char post_buf[1024] = {0};
 
@@ -178,7 +186,7 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
     return ret;
 }
 
-static void _pal_sprout_upload_init(void)
+static void _pal_device_register_init_0(void)
 {
     if (upload_did) 
         free(upload_did);
@@ -188,6 +196,42 @@ static void _pal_sprout_upload_init(void)
 
     upload_did      = NULL;
     upload_diddoc   = NULL;
+
+    if (device_register_key_id) {
+        psa_destroy_key(device_register_key_id);
+	    device_register_key_id = 0;        
+    }
+
+    memset(signature, 0, sizeof(signature)); 
+    memset(signature_str, 0, sizeof(signature_str));
+}
+
+static int _pal_device_register_init(char *did)
+{
+    if (NULL == did)
+        return -1;
+
+    _pal_device_register_init_0();
+
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_SIGN_HASH | PSA_KEY_USAGE_VERIFY_HASH | PSA_KEY_USAGE_EXPORT);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+    psa_set_key_type(&attributes, PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1));
+    psa_set_key_lifetime(&attributes, PSA_KEY_PERSISTENCE_VOLATILE);
+    psa_set_key_bits(&attributes, 256);
+
+    psa_status_t status = psa_import_key( &attributes, secret, sizeof(secret), &device_register_key_id );
+    if (PSA_SUCCESS != status)
+        return -2;
+
+	static size_t  signature_length = 0;
+    status = psa_sign_message(device_register_key_id, PSA_ALG_ECDSA(PSA_ALG_SHA_256), did, strlen(did), signature, sizeof(signature), &signature_length);
+	if (PSA_SUCCESS != status)
+		return -3;
+
+	iotex_utils_convert_hex_to_str(signature , signature_length, signature_str);
+
+    return 0;
 }
 
 void iotex_pal_sprout_device_register_start(char *did, char *diddoc)
@@ -195,13 +239,16 @@ void iotex_pal_sprout_device_register_start(char *did, char *diddoc)
     if (NULL == did || NULL == diddoc)
         return;
     
-    _pal_sprout_upload_init();
-    
-    upload_did    = iotex_utils_device_register_did_upload_prepare(did, 1);
+    int ret = _pal_device_register_init(did);
+    if (ret) {
+        printf("Failed to _pal_device_register_init() ret %d\n", ret);
+    }
+
+    upload_did    = iotex_utils_device_register_did_upload_prepare(did, 1, signature_str, true);
     if (upload_did)
         printf("Upload DID : %s\n", upload_did);
 
-    upload_diddoc = iotex_utils_device_register_diddoc_upload_prepare(diddoc, 1);
+    upload_diddoc = iotex_utils_device_register_diddoc_upload_prepare(diddoc, 1, signature_str, true);
     if (upload_diddoc)
         printf("Upload DIDDoc : %s\n", upload_diddoc);  
 
@@ -217,7 +264,7 @@ void iotex_pal_sprout_device_register_start(char *did, char *diddoc)
 
 void iotex_pal_sprout_device_register_stop(void)
 {
-    _pal_sprout_upload_init();
+    _pal_device_register_init_0();
 }
 
 
